c175b28d0cd9a24757bc9d5ae2c36551cad2f33f
Marlon Tavares <marlontf@gmail.com>
2021-11-11 17:23:53 -0300

    add raw link to updateURL

--- MapaDeCultura/Autorizar.js
+++ MapaDeCultura/Autorizar.js
@@ -6,11 +6,12 @@
 // @author       You
 // @match        https://mapadasculturas.ifnmg.edu.br/b/autorizar
 // @icon         https://www.google.com/s2/favicons?domain=edu.br
+// @updateURL    https://raw.githubusercontent.com/marlontf/scriptsJS/main/MapaDeCultura/Autorizar.js?token=AIRHFOB3HTX2IRWJN6KUFDDBRVI32
 // @grant        none
 // ==/UserScript==
 
-(function() {
-    'use strict';
+(function () {
+   'use strict';
 
-    // Your code here...
+   // Your code here...
 })();
